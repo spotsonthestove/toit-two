3d88037d7c0d688daf605ba88ab6a9208abe953d
spotsonthestove <39865429+spotsonthestove@users.noreply.github.com>
2024-04-12 04:57:26 +1200

    just a line for now

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,58 +1,21 @@
 <script>
 
 
-  import ThreeWheel from '$lib/ThreeWheel.svelte';
-  import TorusComponent from '$lib/TorusComponent.svelte';
-  
-
 
 </script>
 
-<h1>Welcome to Toit</h1>
+<h1>Toit</h1>
 
-<h1> take a bit out of a few things and get a round Toit </h1> 
-<h1>  </h1> 
-
-<div id="toit">   
-<p>This might help. 
-
-We'll go thorugh a few ideas
-
-Mainly the Toit and how that might help timey-wimey focus.
-
-We'll also go through the three wheel and how that might help with the Toit.
-
-The main thing to explian is that the TOIT is an excuse or a habit to get into a frame of mind
-
-so that we use one of the parts of the toit so as to drive throught the other parts of the toit.\
-get into the unenjoy so as to get to the biut that is nice and that might just be washing one window\
-
-
-habits are part of it
-
-evry task is in the calendar
-
-and every calendar entry has at least three reminders haha
-
-</p>  </div>
-
-<div  id="threeWheel">  
-<TorusComponent />
-</div>
-
-<ThreeWheel />
+<h1> This will be something when I get a round Toit </h1> 
 
 
 
-<canvas id="canvas"></canvas>
+
 
 
 
 <style>
-  div {
-    height: 50%;
-    width: 50%;
-  }
+  
 </style>
 
 
