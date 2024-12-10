53bf41e2599a7f8e6b65596f9badf3e366efcf68
spotsonthestove <39865429+spotsonthestove@users.noreply.github.com>
2024-12-11 07:19:56 +1300

    try now

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,33 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
-const config = {
-	// …
-	ssr: {
-		noExternal: ['three', 'troika-three-text']
-	}
-}
-
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	ssr: {
+		noExternal: ['three', 'troika-three-text']
+	},
+	optimizeDeps: {
+		include: [
+			'three',
+			'three/examples/jsm/controls/OrbitControls.js',
+			'three/examples/jsm/controls/DragControls.js'
+		]
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					three: ['three'],
+					'three-controls': [
+						'three/examples/jsm/controls/OrbitControls.js',
+						'three/examples/jsm/controls/DragControls.js'
+					]
+				}
+			}
+		}
 	}
 });
  
